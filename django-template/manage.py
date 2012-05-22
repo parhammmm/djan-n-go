17b47c62be9d98ae4e0182678e27f82919e08c10
Parham Saidi <parham@parha.me>
2012-05-22 16:19:49 +0200

    Update django-template/manage.py

--- django-template/manage.py
+++ django-template/manage.py
@@ -5,12 +5,14 @@ import sys
 if __name__ == "__main__":
 	os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings.development")
 
-	from django.core.management import execute_from_command_line
+
 
 	# Force all new new apps to be put in the apps directory
 	if sys.argv[1] == 'startapp':
 		app_path = os.path.join('apps', sys.argv[2])
 		os.makedirs(app_path)
 		sys.argv.append(app_path)
-
+		
+	from django.core.management import execute_from_command_line
+	
 	execute_from_command_line(sys.argv)
