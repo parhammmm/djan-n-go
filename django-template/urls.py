c0d9cc25fde659d5e06b45bce826ee3c549935eb
Parham Saidi <parham@parha.me>
2012-07-26 06:36:22 +0200

    Fix - Function call typo

--- django-template/urls.py
+++ django-template/urls.py
@@ -3,12 +3,10 @@ from django.conf import settings
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
-# admin.autodiscover()]
+# admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'', include('apps.urls')),
-
-    # url(r'^fashionstop/', include('fashionstop.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
