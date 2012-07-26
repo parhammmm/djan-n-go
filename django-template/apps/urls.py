4579e0eb2c30eb297d35673d203bd890b25e7e06
Parham Saidi <parham@parha.me>
2012-07-26 06:37:03 +0200

    removed useless code

--- django-template/apps/urls.py
+++ django-template/apps/urls.py
@@ -1,9 +1,5 @@
 from django.conf.urls import patterns, include, url
 
-# Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
-
 urlpatterns = patterns('',
     # Examples:
     # url(r'', 'apps.{{ project_name }}.views.home', name='home'),
