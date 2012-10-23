f9a20d743888b114d0a93add5fa45592a96d3a6e
Parham Saidi <parham@parha.me>
2012-10-23 21:55:50 +0100

    Moved the root urls.py into the apps dir

--- django-template/apps/urls.py
+++ django-template/apps/urls.py
@@ -1,6 +1,24 @@
 from django.conf.urls import patterns, include, url
+from django.conf import settings
+from django.views.static import serve
+# Un/comment the next two lines to enable or disable the admin:
+from django.contrib import admin
+
+admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'', 'apps.{{ project_name }}.views.home', name='home'),
+	# Uncomment the admin/doc line below to enable admin documentation:
+	# url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+	# Uncomment the next line to enable the admin:
+	url(r'^admin/', include(admin.site.urls)),
 )
+
+if settings.DEBUG:
+	# Serve static
+	from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+	urlpatterns += staticfiles_urlpatterns()
+
+	# Serve Media
+	urlpatterns += patterns('',
+		(r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT }),
+	)
