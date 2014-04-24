546a981f08f7d7a88c56bcbe66b90a36eaa12cd7
Parham Saidi <parham@parha.me>
2014-04-24 13:19:05 +0000

    removed old unused django template files

--- django-template/common/context_processors.py
+++
@@ -1,9 +0,0 @@
-# Copyright 2012 Parham Saidi. All rights reserved.
-
-from django.conf import settings
-
-def debug_mode(request):
-	return {'debug_mode': settings.DEBUG}
-
-def is_production(request):
-	return {'is_production': settings.IS_PRODUCTION}
--- django-template/common/string.py
+++
@@ -1,7 +0,0 @@
-# Copyright 2012 Parham Saidi. All rights reserved.
-
-import string
-import random
-
-def random_string_generator(size=6, chars=string.ascii_uppercase + string.digits):
-	return ''.join(random.choice(chars) for x in range(size))
